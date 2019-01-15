f071d19456f4858e51769ce953c83ee4fec7e23b
sanjayyaldandi <sanjayyeldandi123@gmail.com>
2019-01-15 16:53:42 +0530

    Add lcm.js file
    the file takes two numbers as input and returns the least common
    multiple of given two numbers.

---
+++ lcm.js
@@ -0,0 +1,18 @@
+let num1 = +process.argv[2];
+let num2 = +process.argv[3];
+
+let limit = Math.max(num1,num2);
+
+let gcd = 1;
+
+for(let index=1;index<=limit;index++){
+  if(num1%index==0 && num2%index==0){
+    gcd = index;
+  }
+}
+
+let answer = num1 / gcd;
+
+let lcm = answer * num2;
+
+console.log(lcm);
